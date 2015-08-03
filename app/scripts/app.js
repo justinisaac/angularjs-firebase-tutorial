a04ca50f4acacc4049b3e02e7e394709c229f10f
justinisaac <justinisaac@rogers.com>
2015-08-02 22:11:19 -0500

    buggy code

---
+++ app/scripts/app.js
@@ -0,0 +1,40 @@
+/* global app:true */
+'use strict';
+
+var app = angular.module('angNewsApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+  'firebase'
+]);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/posts.html',
+      controller: 'PostsCtrl'
+    })
+    .when('/posts/:postId', {
+      templateUrl: 'views/showpost.html',
+      controller: 'PostViewCtrl'
+    })
+    .when('/register', {
+      templateUrl: 'views/register.html',
+      controller: 'AuthCtrl'
+    })
+    .when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'AuthCtrl'
+    })
+    .when('/users/:username', {
+      templateUrl: 'views/profile.html',
+      controller: 'ProfileCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+})
+.constant('FIREBASE_URL', 'https://blinding-heat-9688.firebaseio.com/');
